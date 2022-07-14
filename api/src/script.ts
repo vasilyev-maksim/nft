a91f5afffc52975721ca48754f647805655e2c2e
Maksim Vasilyev <mvasilyev@expertoption.com>
2022-07-14 15:57:12 +0300

    tmp

--- api/src/script.ts
+++ api/src/script.ts
@@ -9,9 +9,40 @@ const collectionsDir = new Directory(join(__dirname, '..', 'collections'));
 const generator = new NFTGenerator(collectionsDir);
 
 const layerFiles = generator.getCollections().flatMap(c => c.getCategories().flatMap(x => x.getAll().map(y => y.file)));
-const target = layerFiles.find(x => x.name.includes('ptrn'))!;
-const dest = target.fid.getSiblingFid(target.name + '.png'); // TODO:
 
-sharp(target.fid.path).resize(1000, 1000).png().toFile(dest.path);
+// const target = layerFiles.find(x => x.name.includes('ptrn'))!;
+// const dest = target.fid.getSiblingFid(target.name + '.png'); // TODO:
 
-console.log(dest.path);
+// sharp(target.fid.path).resize(1000, 1000).png().toFile(dest.path);
+
+// console.log(dest.path);
+const face1 = layerFiles.find(x => x.fullName.startsWith('face1'))!;
+const bg1 = layerFiles.find(x => x.fullName.startsWith('back1'))!;
+
+// sharp(face1.fid.path)
+//   .png()
+//   // .composite([
+//   //   { input: face1.fid.getSiblingFid(face1.name + '.png').path },
+//   //   { input: bg1.fid.getSiblingFid(bg1.name + '.png').path },
+//   // ])
+//   .toFile('./test.png')
+//   .catch(x => console.error('>--- ', x))
+//   .then(() => console.log('end'));
+
+sharp({
+  create: {
+    width: 2000,
+    height: 2000,
+    channels: 4,
+    background: { r: 0, g: 0, b: 0, alpha: 1 },
+  },
+})
+  .png()
+  .composite([
+    { input: bg1.fid.getSiblingFid(bg1.name + '.png').path },
+    { input: face1.fid.getSiblingFid(face1.name + '.png').path },
+  ])
+  .toFile('./test.png')
+  .catch(x => console.error('>--- ', x))
+  .then(() => console.log('end'));
+  find /collections -type f -name '^\d+\.svg$'
